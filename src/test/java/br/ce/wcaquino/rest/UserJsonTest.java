cd2f1fbc14f94f409501bb4c801bd53c867cffcf
sjlpc <saviojrc.1988@gmail.com>
2021-03-05 17:40:12 -0300

    aula16

--- src/test/java/br/ce/wcaquino/rest/UserJsonTest.java
+++ src/test/java/br/ce/wcaquino/rest/UserJsonTest.java
@@ -2,14 +2,24 @@ package br.ce.wcaquino.rest;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.request;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anyOf;
+import static org.hamcrest.Matchers.arrayContaining;
+import static org.hamcrest.Matchers.arrayWithSize;
+import static org.hamcrest.Matchers.closeTo;
+import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.hasItems;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.lessThan;
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
+
+import org.hamcrest.Matchers;
 import org.junit.Test;
 
 import io.restassured.http.Method;
@@ -59,6 +69,7 @@ public class UserJsonTest {
 			.statusCode(200)
 			.body("name", containsString("Joaquina"))
 			.body("endereco.rua", is("Rua dos bobos"));
+		
 			
 	}
 	
@@ -89,7 +100,51 @@ public class UserJsonTest {
 			.get(uri)
 		.then()
 			.statusCode(404)
-			.body("error", is("Usuário inexistente"))
+			.body("error", is("Usuário inexistente"));
+	}
+	
+	@Test
+	public void deveVerificarListaRaiz() {
+		String uri = "http://restapi.wcaquino.me/users";
+		given()
+		.when()
+			.get(uri)
+		.then()
+			.statusCode(200)
+			.body("$",hasSize(3))
+			.body("name", hasItems("João da Silva","Maria Joaquina","Ana Júlia"))
+			.body("age[1]", is(25))
+			.body("filhos.name", hasItem(Arrays.asList("Zezinho","Luizinho")))
+			.body("salary", contains(1234.5678f,2500,null))
+			;
+	}
+	
+	
+	@Test
+	public void devoFazerVerificacoesAvancadas() {
+		String uri = "http://restapi.wcaquino.me/users";
+		given()
+		.when()
+			.get(uri)
+		.then()
+			.statusCode(200)
+			.body("$",hasSize(3))
+			.body("age.findAll{ it <= 25 }.size()", is(2))
+			.body("age.findAll{ it <= 25 && it >20 }.size()", is(1))
+			.body("findAll{ it.age <= 25 && it.age >20 }.name", hasItem("Maria Joaquina"))
+			.body("findAll{ it.age <= 25 }[0].name", is("Maria Joaquina"))
+			.body("findAll{ it.age <= 25 }[-1].name", is("Ana Júlia"))
+			.body("find{ it.age <= 25 }.name", is("Maria Joaquina"))
+			.body("findAll{ it.name.contains('n')}.name",hasItems("Maria Joaquina","Ana Júlia"))
+			.body("findAll{ it.name.length() > 10 }.name",hasItems("João da Silva","Maria Joaquina"))
+			.body("name.collect{ it.toUpperCase() }", hasItem("MARIA JOAQUINA"))
+			.body("name.findAll{ it.startsWith('Maria')}.collect{ it.toUpperCase() }", hasItem("MARIA JOAQUINA"))
+			.body("name.findAll{ it.startsWith('Maria')}.collect{ it.toUpperCase() }.toArray()",anyOf(arrayContaining("MARIA JOAQUINA"),arrayWithSize(1)))
+			.body("age.collect{ it * 2 }",hasItems(60,50,40))
+			.body("id.max()", is(3))
+			.body("salary.min()", is(1234.5678f))
+			.body("salary.findAll{it !=null}.sum()", is(closeTo(3734.5678f, 0.001)))
+			.body("salary.findAll{it !=null}.sum()", allOf(Matchers.greaterThan(3000d),lessThan(5000d)))
 			;
 	}
 }
