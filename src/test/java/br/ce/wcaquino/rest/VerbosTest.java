559ea049d6fd00d0ee5b1d960874b18292359d77
sjlpc <saviojrc.1988@gmail.com>
2021-03-19 17:53:49 -0300

    AULA_27_POST_com_XML

--- src/test/java/br/ce/wcaquino/rest/VerbosTest.java
+++ src/test/java/br/ce/wcaquino/rest/VerbosTest.java
@@ -7,6 +7,8 @@ import static org.hamcrest.Matchers.nullValue;
 
 import org.junit.Test;
 
+import io.restassured.http.ContentType;
+
 public class VerbosTest {
 
 	@Test
@@ -39,5 +41,24 @@ public class VerbosTest {
 			.statusCode(400)
 			.body("id", is(nullValue()))
 			.body("error", is("Name é um atributo obrigatório"));
+	}
+	
+	
+	@Test
+	public void deveSalvarUsuarioViaXML() {
+		given()
+		.log().all()
+		.contentType(ContentType.XML)
+		.body("<user><name>Jose</name><age>50</age></user>")
+	.when()
+		.post("https://restapi.wcaquino.me/usersXML")
+	.then()
+		.log().all()
+		.statusCode(201)
+		.body("user.@id", is(notNullValue()))
+		.body("user.name", is("Jose"))
+		.body("user.age", is("50"));
+		
+		
 	}
 }
